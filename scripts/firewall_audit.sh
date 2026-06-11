7dfdfa4f4ee61783afa4c72b7c51eab9e51aaa7b
harshaldhande <harshal.dhandeof@gmail.com>
2026-06-11 18:32:07 +0000

    added scan feature

--- scripts/firewall_audit.sh
+++ scripts/firewall_audit.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+
+echo "===== FIREWALL AUDIT ====="
+echo ""
+
+echo "[1] UFW Service Status:"
+sudo systemctl is-active ufw
+sudo systemctl is-enabled ufw
+echo ""
+
+echo "[2] UFW Firewall Status:"
 sudo ufw status verbose
+echo ""
+
+echo "===== END OF FIREWALL AUDIT ====="
